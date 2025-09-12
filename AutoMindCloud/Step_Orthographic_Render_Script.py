e2be50c6ec2e6ec5a740621fb80a532495acd218
Artemio A. Day Solvers <78178128+ArtemioA@users.noreply.github.com>
2025-09-12 16:35:31 -0300

    Update Step_Orthographic_Render_Script.py

--- AutoMindCloud/Step_Orthographic_Render_Script.py
+++ AutoMindCloud/Step_Orthographic_Render_Script.py
@@ -1,24 +1,24 @@
 import cadquery as cq
 from cadquery import exporters
-from IPython.display import display, SVG
+from IPython.display import display, SVG,Latex
 
 # Load the STEP file
 #result = cq.importers.importStep('Sketch.step')
 # Function to rotate and display the object
 
-def show_view(result,rotate_axis, rotate_angle, title="Orthographic View"):
+def show_view(result,rotate_axis, rotate_angle, title=""):
     # Rotate the object
     rotated = result.rotate((0,0,0), rotate_axis, rotate_angle)
     # Export as SVG
     svg_str = exporters.getSVG(rotated.val())
     display(SVG(svg_str))
-    IPython.display.Latex("\\text{"+title+"}")
+    Latex("\\text{"+title+"}")
 
 def Step_Orthographic_Render(Sketch_Name):
 
   Sketch_Name = Sketch_Name + str(".step")
   result = cq.importers.importStep(Sketch_Name)
   # Standard views using rotation
-  show_view(result,(1,0,0), 0, IPython.display.Latex("\\text{Front View}"))
-  show_view(result,(1,0,0), 90, IPython.display.Latex("\\text{Top View}"))
-  show_view(result,(0,1,0), 90, IPython.display.Latex("\\text{Right Side View}"))
+  show_view(result,(1,0,0), 0, Latex("\\text{Front View}"))
+  show_view(result,(1,0,0), 90, Latex("\\text{Top View}"))
+  show_view(result,(0,1,0), 90, Latex("\\text{Right Side View}"))
