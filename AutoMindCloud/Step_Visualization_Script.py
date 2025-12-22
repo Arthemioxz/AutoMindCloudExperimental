295068c1a2915fc06e4fd20cee73e913777db08c
Arthemioxz <147190107+Arthemioxz@users.noreply.github.com>
2025-12-22 15:00:10 -0300

    Modify badge element vertical translation
    Adjusted the vertical translation of the badge element in the HTML.

--- AutoMindCloud/Step_Visualization_Script.py
+++ AutoMindCloud/Step_Visualization_Script.py
@@ -149,10 +149,11 @@ def Step_Visualization(Step_Name):
 
   <div id="app"></div>
   <div style="padding-left:20px; overflow:visible;">
-    <div class="badge" style="display:inline-block; transform:scale(2.5) translateX(-15px) translateY(0cm); transform-origin:top left; margin:0 100px 100px 0; overflow:visible;">
+    <div class="badge" style="display:inline-block; transform:scale(2.5) translateX(-15px) translateY(0.5cm); transform-origin:top left; margin:0 100px 100px 0; overflow:visible;">
       <img src="https://raw.githubusercontent.com/Arthemioxz/AutoMindCloudExperimental/main/AutoMindCloud/AutoMindCloud2.png" alt="AutoMind" style="display:block;"/>
     </div>
   </div>
+
 
 
 
