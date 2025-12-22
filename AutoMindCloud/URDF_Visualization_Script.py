eac8ce114af2ef297789fcaf5dc8b011a677b8ff
Arthemioxz <147190107+Arthemioxz@users.noreply.github.com>
2025-12-22 12:50:30 -0300

    Update URDF_Visualization_Script.py

--- AutoMindCloud/URDF_Visualization_Script.py
+++ AutoMindCloud/URDF_Visualization_Script.py
@@ -475,7 +475,7 @@ def URDF_Visualization(
 <body>
   <div id="app"></div>
   <div style="padding-left:20px; overflow:visible;">
-    <div class="badge" style="display:inline-block; transform:scale(2) translateX(-10px); transform-origin:top left; margin:0 50px 50px 0; overflow:visible;">
+    <div class="badge" style="display:inline-block; transform:scale(2) translateX(-15px); transform-origin:top left; margin:0 70px 70px 0; overflow:visible;">
       <img src="https://raw.githubusercontent.com/Arthemioxz/AutoMindCloudExperimental/main/AutoMindCloud/AutoMindCloud2.png" alt="AutoMind" style="display:block;"/>
     </div>
   </div>
